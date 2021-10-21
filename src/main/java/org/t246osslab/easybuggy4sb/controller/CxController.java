167c9e11b384646a4ed467090811b4e2317e98a6
Tyler Agypt <tagypt@gmail.com>
2021-10-21 10:06:50 -0400

    Fixed SCA and KICS and SAST Issues

--- src/main/java/org/t246osslab/easybuggy4sb/controller/CxController.java
+++ src/main/java/org/t246osslab/easybuggy4sb/controller/CxController.java
@@ -19,9 +19,10 @@ public class CxController {
 	// curl localhost:8080/legacy/runCommand/whoami
 	@GetMapping("legacy/runCommand/{cmd}")
 	public String runCommand(@PathVariable String cmd) throws IOException {
-		//if (!Pattern.matches("[0-9A-Za-z]+", cmd)) {  
-		//	return "";
-		//}			
+		if (!Pattern.matches("[0-9A-Za-z]+", cmd)) {  
+			return "";
+		}	
+
 		byte[] buf = new byte[1024];
 		int len = Runtime.getRuntime().exec(cmd).getInputStream().read(buf);
 		return new String(buf, 0, len);
